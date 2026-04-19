267f91a3f8bf172b93d7037339e08539786d69f6
Sean Macharia <Snmuanzi@gmail.com>
2026-04-19 21:36:42 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here!
+
+// DISPALY POSTS IN DOM
+function displayPosts(posts) {
+    const postList = document.querySelector("#post-list")
+
+   
+    posts.forEach(post => {
+        const li = document.createElement("li")
+        
+        const h1 = document.createElement("h1")
+        h1.textContent = post.title
+
+        const p = document.createElement("p")
+        p.textContent = post.body
+
+        li.appendChild(h1)
+        li.appendChild(p)
+        postList.appendChild(li)
+
+    })
+}
+
+// FETCH POSTS
+async function fetchPosts() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const posts = await response.json()
+
+        displayPosts(posts)
+    }
+    catch(error) {
+        console.log("Failed to fetch posts:", error)
+    }
+}
+
+fetchPosts()
